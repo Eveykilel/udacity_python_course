ba7f632ed33b5737edd1cabf243737efa0817a88
Eveykilel <eveykilel@gmail.com>
2016-05-05 12:12:22 +0300

    add new changes

--- speed_of_light.py
+++ speed_of_light.py
@@ -1,4 +1,4 @@
 # speed_of_light = 299792458   meters per second
 # centimeters = 100            one meter is 100 centimeters
 # nanosecond = 1.0/1000000000  one billionth of a second
-print (299792458 * 100 * 1.0/1000000000)
+print (299792458 * 100 * 1.0/1000000000 * 1.0/2.7)
