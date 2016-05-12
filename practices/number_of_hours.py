3b30f8032b88835234f8170a15b1eb1c1ad3d540
Eveykilel <eveykilel@gmail.com>
2016-05-12 21:41:43 +0300

    add new changes

---
+++ practices/number_of_hours.py
@@ -0,0 +1,8 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+
+number_of_hours_in_a_day = 24
+number_of_days_in_a_week = 7
+number_of_weeks = 7
+number_of_hours = (number_of_hours_in_a_day * number_of_days_in_a_week * number_of_weeks)
+
+print number_of_hours
