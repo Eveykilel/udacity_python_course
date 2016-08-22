4b5173cae88a4a90106a887b6d940d5ecd77ac99
Eveykilel <eveykilel@gmail.com>
2016-08-22 12:27:08 +0300

    add new changes

--- procedures/find.py
+++ procedures/find.py
@@ -1,6 +1,20 @@
-def find_second(search, target):
+# Define a procedure, find_second, that takes
+# two strings as its inputs: a search string
+# and a target string. It should return a
+# number that is the position of the second
+# occurrence of the target string in the
+# search string.
 
-	return search.find(target, search.find(target)+ 1)
+def find_second(danton, twister):
+    end_of_first_occurance = danton.find(twister)+len(twister)
+    return danton.find(twister,end_of_first_occurance)
+
+
 
 danton = "De l'audace, encore de l'audace, toujours de l'audace"
 print find_second(danton, 'audace')
+#>>> 25
+
+twister = "she sells seashells by the seashore"
+print find_second(twister,'she')
+#>>> 13
